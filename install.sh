1213af653cd96bf7aab0464bbbaa9145c60fe579
Marcus Legendre <marcus.legendre@gmail.com>
2018-10-31 19:44:04 +0100

    Run cp and rm with verbosity for added transparency

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp ./*.service ~/.config/systemd/user/
-cp ./*.timer ~/.config/systemd/user/
+cp -v ./*.service ~/.config/systemd/user/
+cp -v ./*.timer ~/.config/systemd/user/
 systemctl --user enable gnome-background-change.timer --now
 systemctl --user enable gnome-lockscreen-change.timer --now
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 systemctl --user disable gnome-background-change.timer --now
 systemctl --user disable gnome-lockscreen-change.timer --now
-rm ~/.config/systemd/user/gnome-background-change.service
-rm ~/.config/systemd/user/gnome-background-change.timer
-rm ~/.config/systemd/user/gnome-lockscreen-change.service
-rm ~/.config/systemd/user/gnome-lockscreen-change.timer
+rm -v ~/.config/systemd/user/gnome-background-change.service
+rm -v ~/.config/systemd/user/gnome-background-change.timer
+rm -v ~/.config/systemd/user/gnome-lockscreen-change.service
+rm -v ~/.config/systemd/user/gnome-lockscreen-change.timer
